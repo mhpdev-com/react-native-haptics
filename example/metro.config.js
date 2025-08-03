5a224ee81957fbfdf90bbc293fb20eb64d9cd4a7
mhp23 <hpousty@gmail.com>
2025-08-03 20:55:42 +0330

    chore: cleanup the default module and updating the prettier config

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const { getDefaultConfig } = require('@react-native/metro-config');
-const { withMetroConfig } = require('react-native-monorepo-config');
+const {getDefaultConfig} = require('@react-native/metro-config');
+const {withMetroConfig} = require('react-native-monorepo-config');
 
 const root = path.resolve(__dirname, '..');
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,1 @@
-import Haptics from './NativeHaptics';
-
-export function multiply(a: number, b: number): number {
-  return Haptics.multiply(a, b);
-}
+export {default as Haptics} from './NativeHaptics';
